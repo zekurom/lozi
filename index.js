d14127ec471ae5f6e547446ec057d05709ff6f44
Zekurom <aiden.eckstrom@gmail.com>
2023-04-21 14:36:31 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const axios = require('axios');
+const { MeiliSearch } = require('meilisearch');
+const { query } = require('express');
+
+const client = new MeiliSearch({
+    host: 'https://search.rezi.one/',
+    apiKey: 'e2a1974678b37386fef69bb3638a1fb36263b78a8be244c04795ada0fa250d3d',
+})
+
+// client.index('rezi').search('sonic').then(function (response) {
+//     console.log(response)
+// })
+
+// axios.post('https://search.rezi.one/indexes/rezi/search', {
+//     q: query,
+//     limit: 20
+// }, {
+//     headers: {
+//         Authorization: 'Bearer e2a1974678b37386fef69bb3638a1fb36263b78a8be244c04795ada0fa250d3d',
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json'
+//     }
+// })
+//     .then(function (response) {
+//         console.log(response);
+//         socket.emit('search-results', response.data.hits);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//         socket.emit('search-results', { error: error.message });
+//     });
+
+app.use(express.static(__dirname + '/public'));
+app.use('/anim', express.static(__dirname + '/public2'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('search', (query) => {
+        console.log('searching for:', query);
+
+        axios.get(`https://uhdmovies.vip/?s=${encodeURIComponent(query).replace('%20', '+')}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            }
+        })
+            .then(function (response) {
+                //console.log(response.data);
+                let data = response.data
+                data = data.split('\n')
+                //console.log(data)
+                data.forEach((element) => {
+                    console.log(element)
+                })
+                let results = data.filter(function (element) {
+                    //returning false will remove the element
+                    //if (element.startsWith('<a href="vlc:' || '<a href="https:')) {
+                    if (element.includes('https://uhdmovies.vip/download')) {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+                console.log(results)
+                socket.emit('search-results', results);
+            })
+            .catch(function (error) {
+                socket.emit('search-results', { error: error.message })
+            });
+    });
+
+    socket.on('subsearch', (query) => {
+        console.log('searching for links');
+        console.log(query)
+
+        axios.get(query, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            }
+        })
+            .then(function (response) {
+                //console.log(response.data);
+                let data = response.data
+                data = data.replace('<p style="text-align: center;">', '')
+                data = data.split('\n')
+                data.forEach((element) => {
+                    console.log(element)
+                })
+                let results = data.filter(function (element) {
+                    //returning false will remove the element
+                    //if (element.startsWith('<a href="vlc:' || '<a href="https:')) {
+                    if (element.includes('https://techmny.com') && element.includes('Zip / Pack' || 'G-Drive')) {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+                console.log(results)
+                socket.emit('subsearchresults', results)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    })
+
+    socket.on('animsearch', (query) => {
+        
+
+
+        console.log(results)
+        socket.emit('animsearchresults', results)
+    })
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
